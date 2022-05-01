4ebb0fdfc7c13442abdbca4dee8633446790e971
Wlad <wladengine@yandex.ru>
2022-05-01 17:14:18 +0300

    Added the SalesView
    Added Sales page  XAML and supporting ViewModel. Just scaffolds out of VM, without any actial logic

---
+++ WStoreWPFUserInterface/ViewModels/SalesViewModel.cs
@@ -0,0 +1,128 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WStoreWPFUserInterface.ViewModels
+{
+    internal class SalesViewModel : Screen
+    {
+        private BindingList<string> _products;
+
+        public BindingList<string> Products
+        {
+            get { return _products; }
+            set 
+            { 
+                _products = value; 
+                NotifyOfPropertyChange(() => Products);
+            }
+        }
+
+        private BindingList<string> _cart;
+
+        public BindingList<string> Cart
+        {
+            get { return _cart; }
+            set
+            {
+                _cart = value;
+                NotifyOfPropertyChange(() => Cart);
+            }
+        }
+
+        private int _itemQuantity;
+
+        public int ItemQuantity
+        {
+            get { return _itemQuantity; }
+            set 
+            { 
+                _itemQuantity = value; 
+                NotifyOfPropertyChange(() => ItemQuantity);
+            }
+        }
+
+        public string SubTotal
+        { 
+            get
+            {
+                //TODO: make a calculation
+                return "";
+            }
+        }
+        public string Tax
+        {
+            get
+            {
+                //TODO: make a calculation
+                return "";
+            }
+        }
+        public string Total
+        {
+            get
+            {
+                //TODO: make a calculation
+                return "";
+            }
+        }
+
+        public bool CanAddToCart {
+            get
+            {
+                bool output = false;
+
+                //TODO: make sure that smth is selected
+                //TODO: make sure that is an item quantity
+
+                if (_itemQuantity > 0)
+                    output = true;
+
+                return output;
+
+            }
+        }
+        public void AddToCart()
+        {
+            
+        }
+
+        public bool CanRemoveFromCart
+        {
+            get
+            {
+                bool output = false;
+
+                //TODO: make sure that smth is selected
+
+                return output;
+
+            }
+        }
+        public void RemoveFromCart()
+        {
+
+        }
+
+        public bool CanCheckOut
+        {
+            get
+            {
+                bool output = false;
+
+                //TODO: make sure that smth in the cart
+
+                return output;
+
+            }
+        }
+        public void CheckOut()
+        {
+
+        }
+    }
+}
