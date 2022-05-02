e10b9d8974445216f3ef4ceed8ed9cc05ca1cb0c
Wlad <wladengine@yandex.ru>
2022-05-02 15:46:03 +0300

    Added FK in DB
    Added forgotten FK to tables

--- WStoreData/dbo/Tables/Inventory.sql
+++ WStoreData/dbo/Tables/Inventory.sql
@@ -4,5 +4,6 @@
     [ProductId] INT NOT NULL, 
     [Quantity] INT NOT NULL DEFAULT 1, 
     [PurchasePrice] MONEY NOT NULL, 
-    [PurchaseDate] DATETIME2 NOT NULL DEFAULT GETUTCDATE()
+    [PurchaseDate] DATETIME2 NOT NULL DEFAULT GETUTCDATE(), 
+    CONSTRAINT [FK_Inventory_ToProduct] FOREIGN KEY ([ProductId]) REFERENCES [Product]([Id])
 )
