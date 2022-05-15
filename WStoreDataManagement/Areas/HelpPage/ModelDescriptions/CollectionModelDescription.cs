c6394eb2c83883a71664af937056f3605f2a66d9
wladengine <wladengine@yandex.ru>
2022-05-15 22:24:04 +0300

    Remove old ASP.NET MVC project
    We will not support this old app

--- WStoreDataManagement/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace WStoreDataManagement.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- WStoreDataManagement/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WStoreDataManagement.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
